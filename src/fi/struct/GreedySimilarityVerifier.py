d0720bee97ae4e9a9dba7f6f8de72180d7e3b4cd
AndresPonciano <andres.ponciano123@gmail.com>
2021-03-24 11:52:24 -0400

    starting algorithm 2

--- src/fi/struct/GreedySimilarityVerifier.py
+++ src/fi/struct/GreedySimilarityVerifier.py
@@ -1,4 +1,5 @@
 def rule_gain(rule, string1, string2, all_rules):
+    #might have to change rhs since there can be more than one rhs(r) for any rule r
     rhs = set(rule[1].split(' '))
     set_1 = set(string1.split(' '))
 
@@ -26,6 +27,39 @@ def rule_gain(rule, string1, string2, all_rules):
 
     return len(G)/len(U)
 
+def find_initial_candidates(string1, string2, all_rules):
+    print('in find initial')
+
+    cset = set()
+    for rule in all_rules:
+        rg = rule_gain((rule, all_rules[rule]), string1, string2, all_rules)
+        if rg > 0 and rule in string1:
+            cset.add(rule)
+
+    return cset
+
+def find_candidate_rule_set(string1, string2, all_rules):
+    cset_1 = find_initial_candidates(string1, string2, all_rules)
+    cset_2 = find_initial_candidates(string2, string1, all_rules)
+
+
+    return cset_1, cset_2
+
+def expands(string1, string2, cset_1, cset_2):
+    print('expanding')
+    set_1 = set(string1.split(' '))
+    set_2 = set(string2.split(' '))
+
+    set_1_prime = set_1
+    set_2_prime = set_2
+
+    i = 0
+    # while len(cset_1.union(cset_2)) != 0:
+    while i != 10:
+        print(i)
+        i += 1
+
+
 
 
 if __name__ == "__main__":
@@ -42,6 +76,10 @@ if __name__ == "__main__":
         'Conf': 'Conference',
     }
 
-    rule = ('WH', 'Wireless Health')
+    # rule = ('WH', 'Wireless Health')
+    #this is how we would do a single rule?
+    lhs = 'WH'
+    rhs = ['Wireless Health']
 
-    rule_gain1 = rule_gain(rule, q1, t2, synonymPairs)
+    # rule_gain1 = rule_gain(rule, q1, t2, synonymPairs)
+    idk1, idk2 = find_candidate_rule_set(q1, t2, synonymPairs)
