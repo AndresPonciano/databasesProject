bb79b97a0e104442746beda18f6341610cd9032f
AndresPonciano <andres.ponciano123@gmail.com>
2021-04-10 15:37:27 -0400

    kinda fixed se join

--- src/fi/struct/GreedySimilarityVerifier.py
+++ src/fi/struct/GreedySimilarityVerifier.py
@@ -2,14 +2,13 @@ from Jaccard import jaccard_similarity
 
 def rule_gain(rhs, string1, string2, all_rules):
     rhs = set(rhs)
-    set_1 = set(string1.split(' '))
+    set_1 = set(string1.split(';'))
 
     U = rhs.difference(set_1)
-
     if not U:
         return 0
 
-    set_2 = set(string2.split(' '))
+    set_2 = set(string2.split(';'))
 
     #finding applicable rulles of set_2
     rules_prime = set()
@@ -31,6 +30,7 @@ def rule_gain(rhs, string1, string2, all_rules):
 
 def find_initial_candidates(string1, string2, all_rules):
     # print('in find initial')
+    # print(all_rules)
 
     cset = dict()
     for rule in all_rules:
@@ -38,6 +38,10 @@ def find_initial_candidates(string1, string2, all_rules):
         # print('for rule: ', rule, 'RG IS: ', rg)
         if rg > 0 and rule in string1:
             cset[rule] = all_rules[rule]
+            # if rule in cset.keys():
+                # cset[rule].append(rule)
+            # else:
+                # cset[rule] = all_rules[rule]
             # cset.add(rule)
 
     return cset
@@ -45,12 +49,15 @@ def find_initial_candidates(string1, string2, all_rules):
 def find_candidate_rule_set(string1, string2, all_rules):
     #change these to dictionaries??
     cset_1 = find_initial_candidates(string1, string2, all_rules)
+    # print('---')
     cset_2 = find_initial_candidates(string2, string1, all_rules)
 
+    # print('initial cand', cset_1)
+    # print(cset_2)
     #THIS SHOULD BE THE BEGINNING OF A WHILE TRUE LOOP############################
     while True:
-        set_1_prime = set(string1.split(' '))
-        set_2_prime = set(string1.split(' '))
+        set_1_prime = set(string1.split(';'))
+        set_2_prime = set(string1.split(';'))
         for rule in cset_1:
             set_1_prime = set_1_prime.union(set(cset_1[rule]))
 
@@ -81,8 +88,8 @@ def find_candidate_rule_set(string1, string2, all_rules):
         jaccard_of_expanded = expands(string1, string2, temp_cset_1, temp_cset_2, all_rules)
         # print('returned from expands: ', jaccard_of_expanded)
         # print('after expands: ')
-        print(temp_cset_1)
-        print(temp_cset_2)
+        # print(temp_cset_1)
+        # print(temp_cset_2)
         if (not temp_cset_1 and not temp_cset_2) or (temp_cset_1 == cset_1 and temp_cset_2 == cset_2):
             break
 
@@ -90,11 +97,11 @@ def find_candidate_rule_set(string1, string2, all_rules):
         cset_2 = temp_cset_2.copy()
     #THIS SHOULD BE THE END OF THE WHILE TRUE LOOP###############################
 
-    print('before returning from find c_set', set_1_prime)
+    # print('before returning from find c_set', set_1_prime)
     return cset_1, cset_2
 
 def find_gain_effective_rule(cset, string1, string2, all_rules):
-    print('finding current most gain-effective rule')
+    # print('finding current most gain-effective rule')
 
     max_gain_rule = 0.0
     lhs, rhs = None, None
@@ -108,9 +115,9 @@ def find_gain_effective_rule(cset, string1, string2, all_rules):
     return lhs, rhs, max_gain_rule
 
 def expands(string1, string2, cset_1, cset_2, all_rules):
-    print('expanding')
-    set_1_prime = set(string1.split(' '))
-    set_2_prime = set(string2.split(' '))
+    # print('expanding')
+    set_1_prime = set(string1.split(';'))
+    set_2_prime = set(string2.split(';'))
 
 
     i = 0
@@ -132,15 +139,17 @@ def expands(string1, string2, cset_1, cset_2, all_rules):
         if lhs2 in cset_2.keys():
             del cset_2[lhs2]
 
-        print(i)
+        # print(i)
         i += 1
     
+    # print('in expands', set_1_prime)
+    # print(set_2_prime)
     return jaccard_similarity(set_1_prime, set_2_prime)
 
 if __name__ == "__main__":
 
-    # q1 = 'Intl WH Conf 2012'
-    # t2 = 'Intl Wireless Health Conference 2012 UK'
+    # q1 = 'Intl;WH;Conf;2012'
+    # t2 = 'Intl;WirelessHealth;Conference;2012;UK'
 
     # synonymPairs = {
     #     'WH': ['Wireless Health'],
@@ -155,15 +164,22 @@ if __name__ == "__main__":
     # idk1, idk2 = find_candidate_rule_set(q1, t2, synonymPairs)
     # print(idk1, idk2)
 
-    s1 = 'Proceedings of the VLDB Endowment 2012: 38th International Conference on Very Large Databases, Turkey'
-    s2 = 'PVLDB 2012 Turkey'
+    s1 = 'Proceedings of the VLDB Endowment;2012;38th;International Conference on Very Large Databases;Turkey'
+    s2 = 'PVLDB;2012;Turkey'
 
     synonymPairs = {
-        'PVLDB': ['International Conference on Very Large Databases'],
-        'PVLDB': ['Proceedings of the VLDB Endowment'],
+        'PVLDB': ['International Conference on Very Large Databases', 'Proceedings of the VLDB Endowment'],
+        # 'PVLDB': ['Proceedings of the VLDB Endowment'],
     }
 
     idk1, idk2 = find_candidate_rule_set(s1, s2, synonymPairs)
+    # print('hi', idk1, idk2)
     theta = expands(s1, s2, idk1, idk2, synonymPairs)
-    print('hi', idk1, idk2)
     print(theta)
+
+    set1 = set(s1.split(';'))
+    set2 = set(s2.split(';'))
+    # print('bruh', set1)
+    # print(set2)
+    bruh = jaccard_similarity(set1, set2)
+    print(bruh)
